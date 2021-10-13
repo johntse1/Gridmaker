6d2446ded47e882037c5eb9316e457cb326f4684
John Tse <jtse63@gmail.com>
2021-10-13 03:07:45 -0400

    Completed addRow
    Add row was finished

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ let colorSelected;
 
 //Add a row
 function addR() {
-    alert("Clicked Add Row")
+    alert("Clicked add row")
+    var table = document.getElementById("grid");
+    //Creates first element in the table
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    alert(numRows)
+    //Loops through the number of columns to add rows
+    for(let i =0; i < numCols;i++){
+        var cell1 = row.insertCell();
+    }
+    cell1.innerHTML = numRows.toString();
+    numRows++;
 }
 //Add a column
 function addC() {
-    alert("Clicked Add Col")
+    alert("Clicked add Column")
+    var table = document.getElementById("grid");
+    //first element in table
+    var col = table.insertRow(0);
+    var cell2 = col.insertCell(0);
+    for(let i=0;i< numRows;i++){
+        var cell2 = col[i].insertCell();
+    }
+    cell2.innerHTML = numCols.toString();
+    numCols++;
 }
 
 //Remove a row
